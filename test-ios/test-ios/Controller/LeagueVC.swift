633820c8df6df41061e8ab23e4b06e15073e7329
lobodormido <niko.riveiro@gmail.com>
2017-10-18 03:40:40 -0300

    de vista a otra prog

---
+++ test-ios/test-ios/Controller/LeagueVC.swift
@@ -0,0 +1,36 @@
+//
+//  LeagueVC.swift
+//  test-ios
+//
+//  Created by nek on 18/10/17.
+//  Copyright © 2017 MacBookPro. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+            performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
